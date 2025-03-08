856898bd9823f632a18c8184771e1a78df03418c
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-08 21:43:00 +0900

    add input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Button, HStack, Heading } from '@chakra-ui/react'
+import { Button, HStack, Heading, Input } from '@chakra-ui/react'
+import { useState } from 'react'
 
 function App() {
+  const [input, setInput] = useState('')
+
   return (
     <>
       <div className="container">
+        <section>
+          <Heading size="4xl">Input</Heading>
+          <Input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <p>{input}</p>
+        </section>
+
         <section>
           <Heading size="4xl">Button</Heading>
           <HStack>

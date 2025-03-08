f85abebabda01746b3a5aa4d9ec4e74c4f4cc1b6
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-08 21:40:47 +0900

    add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Button, HStack, Heading } from '@chakra-ui/react'
+
 function App() {
   return (
     <>
-      <div className="container">Contents</div>
+      <div className="container">
+        <section>
+          <Heading size="4xl">Button</Heading>
+          <HStack>
+            <Button>Click me</Button>
+            <Button>Click me</Button>
+          </HStack>
+        </section>
+      </div>
     </>
   )
 }
